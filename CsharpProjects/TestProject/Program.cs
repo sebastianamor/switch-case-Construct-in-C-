b6585c555e8e8b7ca78d04c95680df3c8bd2adf4
sebastian lapt <smoreira11@hotmail.com>
2025-06-03 13:09:16 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,52 +1,9 @@
 ﻿// SKU = Stock Keeping Unit. 
 // SKU value format: <product #>-<2-letter color code>-<size code>
-string  sku = "01-MN-L";
+string sku = "01-MN-L";
 
 string[] product = sku.Split('-');
 
 string type = "";
 string color = "";
 string size = "";
-
-switch(product) {  
-     
-      case (product[0] == "01")
-      type = "Sweat shirt";
-      break;
-
-      case (product[0] == "02")
-      type = "T-Shirt";
-      break; 
-
-
-     case  (product[0] == "03")
-    type = "Sweat pants";
-     break;
-
-    type = "Other";
-   case (product[1] == "BL")
-    color = "Black";
-    break;
-    case (product[1] == "MN")
-    color = "Maroon";
-    break;
-
-    color = "White";
-
-
-  case  (product[2] == "S")
-    size = "Small";
-    break;
-
-
-   case (product[2] == "M")
-    size = "Medium";
-    break;
-
-   case (product[2] == "L")
-    size = "Large";
-    break;
-    size = "One Size Fits All";
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
